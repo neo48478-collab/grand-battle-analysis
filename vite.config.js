554a40cb9022ff37b34cd538e7947056be2b0919
neo48478-collab <neo48478-collab@users.noreply.github.com>
2026-08-12 19:38:30 +0900

    fix: use relative assets in Android app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/grand-battle-analysis/',
-})
+  base: mode === 'android' ? './' : '/grand-battle-analysis/',
+}))
